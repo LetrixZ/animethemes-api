2be55f05f7e957d5568990e108cdbdeca2c8d932
Fermin Cirella <letrix@outlook.es>
2020-07-26 16:28:29 -0300

    Changes

--- src/app.py
+++ src/app.py
@@ -28,8 +28,8 @@ def create_app(environment):
     return app
 
 
-# environment = config['production']
-environment = config['development']
+environment = config['production']
+# environment = config['development']
 
 app = create_app(environment)
 ApiDoc(app=app)
