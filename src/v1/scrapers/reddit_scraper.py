c9c7042fc84165ebec39cc0359d07212ff735840
Fermín Cirella <letrix@outlook.es>
2020-09-27 17:27:27 -0300

    Update reddit_scraper.py

--- src/v1/scrapers/reddit_scraper.py
+++ src/v1/scrapers/reddit_scraper.py
@@ -5,8 +5,8 @@ from mal import Anime as AnimeMAL
 
 from models import Anime, Theme
 
-reddit = praw.Reddit(client_id="mS1uQkjEv2vxhg",
-                     client_secret="Vs9q60YyROx780avM7AqsVFzfYM",
+reddit = praw.Reddit(client_id=os.getenv('CLIENT_ID'),
+                     client_secret=os.getenv('CLIENT_SECRET'),
                      user_agent="Letrix's AnimeThemes API")
 
 
